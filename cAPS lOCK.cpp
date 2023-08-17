ec4f274de60e88ea5c3e1240d96d03cc926fe3ff
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-08-17 20:48:23 +0600

    Add files via upload

---
+++ cAPS lOCK.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int c=0,r=0;
+string s;
+cin>>s;
+for(int i=1;i<s.size();i++){
+
+   if(islower(s[i])){
+
+    c++;
+   }
+}
+for(int i =0; i<s.size();i++){
+
+    if(isupper(s[i])){
+        r++;
+    }
+}
+   if(islower(s[0])&&c==0) {
+
+    s[0]=toupper(s[0]);
+    for(int i=1; i<s.size();i++){
+
+        s[i]=tolower(s[i]);
+    }
+    cout<<s;
+return 0;
+   }
+   if(r==s.size()){
+    for(int i =0; i<s.size();i++){
+
+        s[i]=tolower(s[i]);
+    }
+    cout<<s;
+   }
+   else{
+    cout<<s;
+
+   }
+}
