6c72b954ccfffd83ea51d1f9fd342716550acdca
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-08-20 17:41:12 +0600

    Add files via upload

---
+++ In Search of an Easy Problem.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, count=0;
+    cin>>a;
+    int ar[a];
+    for(int i =0; i<a;i++){
+
+        cin>>ar[i];
+        if(ar[i]==1){
+            count++;
+        }
+    }
+    if(count==0){
+
+        cout<<"easy ";
+    }
+    else{
+
+        cout<<"Hard";
+    }
+}
